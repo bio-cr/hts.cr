126a161d0046b1ee0859f605e3e14d1dcb0482e3
kojix2 <2xijok@gmail.com>
2021-12-28 01:23:09 +0900

    Add bcf.cr [wip]

--- src/hts/bcf.cr
+++ src/hts/bcf.cr
@@ -8,5 +8,31 @@ require "./bcf/record"
 
 module HTS
   class Bcf
+    include Enumerable(Bcf::Record)
+
+    getter :header
+    getter :htf_file
+
+    def initialize(filename : Path | String)
+      file_path = File.expand_path(filename).to_s
+
+      unless File.exists?(file_path)
+        raise "File not found: #{file_path}"
+      end
+
+      @htf_file = LibHTS.hts_open(file_path, "r")
+      @header   = Bcf::Header.new(LibHTS.bcf_hdr_read(htf_file))
+    end
+
+    # Close the current file.
+    def close
+      LibHTS.hts_close(htf_file)
+    end
+
+    def each
+      while LibHTS.bcf_read(htf_file, header.struct, bcf1 = LibHTS.bcf_init) > 0
+        yield Bcf::Record.new(bcf1, header)
+      end
+    end   
   end
 end
