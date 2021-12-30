dc8f76b26c151d678fb3fee5eae3d9b11cfc9b73
kojix2 <2xijok@gmail.com>
2021-12-30 20:37:24 +0900

    Added methods to BCF::Record

--- examples/bcf.cr
+++ examples/bcf.cr
@@ -5,7 +5,10 @@ bcf_path = File.expand_path("../../test/fixtures/test.bcf", __FILE__)
 bcf = HTS::Bcf.new(bcf_path)
 
 bcf.each do |r|
-  p pos: r.pos
+  p pos: r.pos,
+    start: r.start,
+    stop: r.stop,
+    id: r.id
 end
 
 bcf.close
--- src/hts/bcf/record.cr
+++ src/hts/bcf/record.cr
@@ -9,6 +9,19 @@ module HTS
       def pos
         @bcf1.value.pos + 1
       end
+
+      def start
+        @bcf1.value.pos
+      end
+
+      def stop
+        @bcf1.value.pos + @bcf1.value.rlen
+      end
+
+      def id
+        LibHTS.bcf_unpack(@bcf1, LibHTS2::BCF_UN_INFO)
+        String.new(@bcf1.value.d.id)
+      end
     end
   end
 end
