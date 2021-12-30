16bd2a55823b12f7e36e9d0fe39398ec651e0e4f
kojix2 <2xijok@gmail.com>
2021-12-30 13:32:37 +0900

    Add teardown to bam test

--- test/hts/bam_test.cr
+++ test/hts/bam_test.cr
@@ -2,11 +2,15 @@ require "minitest/autorun"
 require "../../src/hts/bam"
 
 class BamTest < Minitest::Test
-  def bam
-    HTS::Bam.new(File.expand_path("../fixtures/poo.sort.bam", __DIR__))
+  def setup
+    @bam = HTS::Bam.new(File.expand_path("../fixtures/poo.sort.bam", __DIR__))
   end
 
-  def test_close
-    assert_equal 0, bam.close
+  def teardown
+    @bam.try &.close
+  end
+
+  def test_initialize
+    assert_instance_of HTS::Bam, @bam
   end
 end
