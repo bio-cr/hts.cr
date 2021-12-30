a84c464def8da24c26163d1f810cfde5a52b42c1
kojix2 <2xijok@gmail.com>
2021-12-30 15:15:07 +0900

    Update libhts.cr

--- src/hts/libhts/libhts.cr
+++ src/hts/libhts/libhts.cr
@@ -1,6 +1,9 @@
 module HTS
   @[Link("htslib")]
   lib LibHTS
+    BCF_GT_MISSING     = 0
+    BCF_STR_VECTOR_END = 0
+    BCF_STR_MISSING    = 7
     alias HtsTpool = Void
     alias BgzfMtauxT = Void
     alias BgzfCacheT = Void*
@@ -710,7 +713,26 @@ module HTS
     fun tbx_index_load3(fn : LibC::Char*, fnidx : LibC::Char*, flags : LibC::Int) : TbxT*
     fun tbx_seqnames(tbx : TbxT*, n : LibC::Int*) : LibC::Char**
     fun tbx_destroy(tbx : TbxT*)
-    fun vcf_parse(s : KstringT*, h : BcfHdrT*, v : Bcf1T*) : LibC::Int
+
+    struct BcfHrecT
+      type : LibC::Int
+      key : LibC::Char*
+      value : LibC::Char*
+      nkeys : LibC::Int
+      keys : LibC::Char**
+      vals : LibC::Char**
+    end
+
+    struct BcfIdinfoT
+      info : Uint64T[3]
+      hrec : BcfHrecT*[3]
+      id : LibC::Int
+    end
+
+    struct BcfIdpairT
+      key : LibC::Char*
+      val : BcfIdinfoT*
+    end
 
     struct BcfHdrT
       n : Int32T[3]
@@ -728,44 +750,38 @@ module HTS
       m : Int32T[3]
     end
 
-    struct BcfIdpairT
-      key : LibC::Char*
-      val : BcfIdinfoT*
-    end
-
-    struct BcfIdinfoT
-      info : Uint64T[3]
-      hrec : BcfHrecT*[3]
-      id : LibC::Int
-    end
-
-    struct BcfHrecT
+    struct BcfVariantT
       type : LibC::Int
-      key : LibC::Char*
-      value : LibC::Char*
-      nkeys : LibC::Int
-      keys : LibC::Char**
-      vals : LibC::Char**
+      n : LibC::Int
     end
 
-    struct Bcf1T
-      pos : HtsPosT
-      rlen : HtsPosT
-      rid : Int32T
-      qual : LibC::Float
-      n_info_allele : Uint32T # FIXME
-      # n_info : Uint32T
-      # n_allele : Uint32T
-      n_fmt_sample : Uint32T # FIXME
-      # n_fmt : Uint32T
-      # n_sample : Uint32T
-      shared : KstringT
-      indiv : KstringT
-      d : BcfDecT
-      max_unpack : LibC::Int
-      unpacked : LibC::Int
-      unpack_size : LibC::Int[3]
-      errcode : LibC::Int
+    struct BcfFmtT
+      id : LibC::Int
+      n : LibC::Int
+      size : LibC::Int
+      type : LibC::Int
+      p : Uint8T*
+      p_len : Uint32T
+      p_off_free : Uint32T # FIXME
+      # p_off : Uint32T
+      # p_free : Uint32T
+    end
+
+    struct BcfInfoT
+      key : LibC::Int
+      type : LibC::Int
+      v1 : BcfInfoTV1
+      vptr : Uint8T*
+      vptr_len : Uint32T
+      vptr_off_free : Uint32T # FIXME
+      # vptr_off : Uint32T
+      # vptr_free : Uint32T
+      len : LibC::Int
+    end
+
+    union BcfInfoTV1
+      i : Int64T
+      f : LibC::Float
     end
 
     struct BcfDecT
@@ -789,47 +805,130 @@ module HTS
       indiv_dirty : LibC::Int
     end
 
-    struct BcfInfoT
-      key : LibC::Int
-      type : LibC::Int
-      v1 : BcfInfoTV1
-      vptr : Uint8T*
-      vptr_len : Uint32T
-      vptr_off_free : Uint32T # FIXME
-      # vptr_off : Uint32T
-      # vptr_free : Uint32T
-      len : LibC::Int
+    fun bcf_hdr_init(mode : LibC::Char*) : BcfHdrT*
+    fun bcf_hdr_destroy(h : BcfHdrT*)
+    fun bcf_init : Bcf1T*
+
+    struct Bcf1T
+      pos : HtsPosT
+      rlen : HtsPosT
+      rid : Int32T
+      qual : LibC::Float
+      n_info_allele : Uint32T # FIXME
+      # n_info : Uint32T
+      # n_allele : Uint32T
+      n_fmt_sample : Uint32T # FIXME
+      # n_fmt : Uint32T
+      # n_sample : Uint32T
+      shared : KstringT
+      indiv : KstringT
+      d : BcfDecT
+      max_unpack : LibC::Int
+      unpacked : LibC::Int
+      unpack_size : LibC::Int[3]
+      errcode : LibC::Int
     end
 
-    union BcfInfoTV1
-      i : Int64T
-      f : LibC::Float
-    end
-
-    struct BcfFmtT
-      id : LibC::Int
-      n : LibC::Int
-      size : LibC::Int
-      type : LibC::Int
-      p : Uint8T*
-      p_len : Uint32T
-      p_off_free : Uint32T # FIXME
-      # p_off : Uint32T
-      # p_free : Uint32T
-    end
-
-    struct BcfVariantT
-      type : LibC::Int
-      n : LibC::Int
-    end
-
+    fun bcf_destroy(v : Bcf1T*)
+    fun bcf_empty(v : Bcf1T*)
+    fun bcf_clear(v : Bcf1T*)
+    fun bcf_hdr_read(fp : HtsFile*) : BcfHdrT*
+    fun bcf_hdr_set_samples(hdr : BcfHdrT*, samples : LibC::Char*, is_file : LibC::Int) : LibC::Int
+    fun bcf_subset_format(hdr : BcfHdrT*, rec : Bcf1T*) : LibC::Int
+    fun bcf_hdr_write(fp : HtsFile*, h : BcfHdrT*) : LibC::Int
+    fun vcf_parse(s : KstringT*, h : BcfHdrT*, v : Bcf1T*) : LibC::Int
     fun vcf_open_mode(mode : LibC::Char*, fn : LibC::Char*, format : LibC::Char*) : LibC::Int
     fun vcf_format(h : BcfHdrT*, v : Bcf1T*, s : KstringT*) : LibC::Int
+    fun bcf_read(fp : HtsFile*, h : BcfHdrT*, v : Bcf1T*) : LibC::Int
+    fun bcf_unpack(b : Bcf1T*, which : LibC::Int) : LibC::Int
+    fun bcf_dup(src : Bcf1T*) : Bcf1T*
+    fun bcf_copy(dst : Bcf1T*, src : Bcf1T*) : Bcf1T*
+    fun bcf_write(fp : HtsFile*, h : BcfHdrT*, v : Bcf1T*) : LibC::Int
     fun vcf_hdr_read(fp : HtsFile*) : BcfHdrT*
     fun vcf_hdr_write(fp : HtsFile*, h : BcfHdrT*) : LibC::Int
     fun vcf_read(fp : HtsFile*, h : BcfHdrT*, v : Bcf1T*) : LibC::Int
     fun vcf_write(fp : HtsFile*, h : BcfHdrT*, v : Bcf1T*) : LibC::Int
+    fun bcf_readrec(fp : Bgzf*, null : Void*, v : Void*, tid : LibC::Int*, beg : HtsPosT*, _end : HtsPosT*) : LibC::Int
     fun vcf_write_line(fp : HtsFile*, line : KstringT*) : LibC::Int
+    fun bcf_hdr_dup(hdr : BcfHdrT*) : BcfHdrT*
+    fun bcf_hdr_combine(dst : BcfHdrT*, src : BcfHdrT*) : LibC::Int
+    fun bcf_hdr_merge(dst : BcfHdrT*, src : BcfHdrT*) : BcfHdrT*
+    fun bcf_hdr_add_sample(hdr : BcfHdrT*, sample : LibC::Char*) : LibC::Int
+    fun bcf_hdr_set(hdr : BcfHdrT*, fname : LibC::Char*) : LibC::Int
+    fun bcf_hdr_format(hdr : BcfHdrT*, is_bcf : LibC::Int, str : KstringT*) : LibC::Int
+    fun bcf_hdr_fmt_text(hdr : BcfHdrT*, is_bcf : LibC::Int, len : LibC::Int*) : LibC::Char*
+    fun bcf_hdr_append(h : BcfHdrT*, line : LibC::Char*) : LibC::Int
+    fun bcf_hdr_printf(h : BcfHdrT*, format : LibC::Char*, ...) : LibC::Int
+    fun bcf_hdr_get_version(hdr : BcfHdrT*) : LibC::Char*
+    fun bcf_hdr_set_version(hdr : BcfHdrT*, version : LibC::Char*) : LibC::Int
+    fun bcf_hdr_remove(h : BcfHdrT*, type : LibC::Int, key : LibC::Char*)
+    fun bcf_hdr_subset(h0 : BcfHdrT*, n : LibC::Int, samples : LibC::Char**, imap : LibC::Int*) : BcfHdrT*
+    fun bcf_hdr_seqnames(h : BcfHdrT*, nseqs : LibC::Int*) : LibC::Char**
+    fun bcf_hdr_parse(hdr : BcfHdrT*, htxt : LibC::Char*) : LibC::Int
+    fun bcf_hdr_sync(h : BcfHdrT*) : LibC::Int
+    fun bcf_hdr_parse_line(h : BcfHdrT*, line : LibC::Char*, len : LibC::Int*) : BcfHrecT*
+    fun bcf_hrec_format(hrec : BcfHrecT*, str : KstringT*) : LibC::Int
+    fun bcf_hdr_add_hrec(hdr : BcfHdrT*, hrec : BcfHrecT*) : LibC::Int
+    fun bcf_hdr_get_hrec(hdr : BcfHdrT*, type : LibC::Int, key : LibC::Char*, value : LibC::Char*, str_class : LibC::Char*) : BcfHrecT*
+    fun bcf_hrec_dup(hrec : BcfHrecT*) : BcfHrecT*
+    fun bcf_hrec_add_key(hrec : BcfHrecT*, str : LibC::Char*, len : LibC::SizeT) : LibC::Int
+    fun bcf_hrec_set_val(hrec : BcfHrecT*, i : LibC::Int, str : LibC::Char*, len : LibC::SizeT, is_quoted : LibC::Int) : LibC::Int
+    fun bcf_hrec_find_key(hrec : BcfHrecT*, key : LibC::Char*) : LibC::Int
+    fun bcf_hrec_destroy(hrec : BcfHrecT*)
+    fun bcf_subset(h : BcfHdrT*, v : Bcf1T*, n : LibC::Int, imap : LibC::Int*) : LibC::Int
+    fun bcf_translate(dst_hdr : BcfHdrT*, src_hdr : BcfHdrT*, src_line : Bcf1T*) : LibC::Int
+    fun bcf_get_variant_types(rec : Bcf1T*) : LibC::Int
+    fun bcf_get_variant_type(rec : Bcf1T*, ith_allele : LibC::Int) : LibC::Int
+    fun bcf_is_snp(v : Bcf1T*) : LibC::Int
+    fun bcf_update_filter(hdr : BcfHdrT*, line : Bcf1T*, flt_ids : LibC::Int*, n : LibC::Int) : LibC::Int
+    fun bcf_add_filter(hdr : BcfHdrT*, line : Bcf1T*, flt_id : LibC::Int) : LibC::Int
+    fun bcf_remove_filter(hdr : BcfHdrT*, line : Bcf1T*, flt_id : LibC::Int, pass : LibC::Int) : LibC::Int
+    fun bcf_has_filter(hdr : BcfHdrT*, line : Bcf1T*, filter : LibC::Char*) : LibC::Int
+    fun bcf_update_alleles(hdr : BcfHdrT*, line : Bcf1T*, alleles : LibC::Char**, nals : LibC::Int) : LibC::Int
+    fun bcf_update_alleles_str(hdr : BcfHdrT*, line : Bcf1T*, alleles_string : LibC::Char*) : LibC::Int
+    fun bcf_update_id(hdr : BcfHdrT*, line : Bcf1T*, id : LibC::Char*) : LibC::Int
+    fun bcf_add_id(hdr : BcfHdrT*, line : Bcf1T*, id : LibC::Char*) : LibC::Int
+    fun bcf_update_info(hdr : BcfHdrT*, line : Bcf1T*, key : LibC::Char*, values : Void*, n : LibC::Int, type : LibC::Int) : LibC::Int
+    fun bcf_update_info_int64(hdr : BcfHdrT*, line : Bcf1T*, key : LibC::Char*, values : Int64T*, n : LibC::Int) : LibC::Int
+    fun bcf_update_format_string(hdr : BcfHdrT*, line : Bcf1T*, key : LibC::Char*, values : LibC::Char**, n : LibC::Int) : LibC::Int
+    fun bcf_update_format(hdr : BcfHdrT*, line : Bcf1T*, key : LibC::Char*, values : Void*, n : LibC::Int, type : LibC::Int) : LibC::Int
+    fun bcf_gt2alleles(igt : LibC::Int, a : LibC::Int*, b : LibC::Int*)
+    fun bcf_get_fmt(hdr : BcfHdrT*, line : Bcf1T*, key : LibC::Char*) : BcfFmtT*
+    fun bcf_get_info(hdr : BcfHdrT*, line : Bcf1T*, key : LibC::Char*) : BcfInfoT*
+    fun bcf_get_fmt_id(line : Bcf1T*, id : LibC::Int) : BcfFmtT*
+    fun bcf_get_info_id(line : Bcf1T*, id : LibC::Int) : BcfInfoT*
+    fun bcf_get_info_values(hdr : BcfHdrT*, line : Bcf1T*, tag : LibC::Char*, dst : Void**, ndst : LibC::Int*, type : LibC::Int) : LibC::Int
+    fun bcf_get_info_int64(hdr : BcfHdrT*, line : Bcf1T*, tag : LibC::Char*, dst : Int64T**, ndst : LibC::Int*) : LibC::Int
+    fun bcf_get_format_string(hdr : BcfHdrT*, line : Bcf1T*, tag : LibC::Char*, dst : LibC::Char***, ndst : LibC::Int*) : LibC::Int
+    fun bcf_get_format_values(hdr : BcfHdrT*, line : Bcf1T*, tag : LibC::Char*, dst : Void**, ndst : LibC::Int*, type : LibC::Int) : LibC::Int
+    fun bcf_hdr_id2int(hdr : BcfHdrT*, type : LibC::Int, id : LibC::Char*) : LibC::Int
+    fun bcf_hdr_name2id(hdr : BcfHdrT*, id : LibC::Char*) : LibC::Int
+    fun bcf_hdr_id2name(hdr : BcfHdrT*, rid : LibC::Int) : LibC::Char*
+    fun bcf_seqname(hdr : BcfHdrT*, rec : Bcf1T*) : LibC::Char*
+    fun bcf_seqname_safe(hdr : BcfHdrT*, rec : Bcf1T*) : LibC::Char*
+    fun bcf_fmt_array(s : KstringT*, n : LibC::Int, type : LibC::Int, data : Void*) : LibC::Int
+    fun bcf_fmt_sized_array(s : KstringT*, ptr : Uint8T*) : Uint8T*
+    fun bcf_enc_vchar(s : KstringT*, l : LibC::Int, a : LibC::Char*) : LibC::Int
+    fun bcf_enc_vint(s : KstringT*, n : LibC::Int, a : Int32T*, wsize : LibC::Int) : LibC::Int
+    fun bcf_enc_vfloat(s : KstringT*, n : LibC::Int, a : LibC::Float*) : LibC::Int
+    fun bcf_itr_next(htsfp : HtsFile*, itr : HtsItrT*, r : Void*) : LibC::Int
+    fun bcf_index_load2(fn : LibC::Char*, fnidx : LibC::Char*) : HtsIdxT
+    fun bcf_index_load3(fn : LibC::Char*, fnidx : LibC::Char*, flags : LibC::Int) : HtsIdxT
+    fun bcf_index_build(fn : LibC::Char*, min_shift : LibC::Int) : LibC::Int
+    fun bcf_index_build2(fn : LibC::Char*, fnidx : LibC::Char*, min_shift : LibC::Int) : LibC::Int
+    fun bcf_index_build3(fn : LibC::Char*, fnidx : LibC::Char*, min_shift : LibC::Int, n_threads : LibC::Int) : LibC::Int
+    fun bcf_idx_init(fp : HtsFile*, h : BcfHdrT*, min_shift : LibC::Int, fnidx : LibC::Char*) : LibC::Int
+    fun bcf_idx_save(fp : HtsFile*) : LibC::Int
+    fun bcf_float_set(ptr : LibC::Float*, value : Uint32T)
+    fun bcf_float_is_missing(f : LibC::Float) : LibC::Int
+    fun bcf_float_is_vector_end(f : LibC::Float) : LibC::Int
+    fun bcf_format_gt(fmt : BcfFmtT*, isample : LibC::Int, str : KstringT*) : LibC::Int
+    fun bcf_enc_size(s : KstringT*, size : LibC::Int, type : LibC::Int) : LibC::Int
+    fun bcf_enc_inttype(x : LibC::Long) : LibC::Int
+    fun bcf_enc_int1(s : KstringT*, x : Int32T) : LibC::Int
+    fun bcf_dec_int1(p : Uint8T*, type : LibC::Int, q : Uint8T**) : Int64T
+    fun bcf_dec_typed_int1(p : Uint8T*, q : Uint8T**) : Int64T
+    fun bcf_dec_size(p : Uint8T*, q : Uint8T**, type : LibC::Int*) : Int32T
     $hts_verbose : LibC::Int
     $bam_cigar_table : Int8T[256]
     $tbx_conf_gff : TbxConfT
@@ -837,5 +936,8 @@ module HTS
     $tbx_conf_psltbl : TbxConfT
     $tbx_conf_sam : TbxConfT
     $tbx_conf_vcf : TbxConfT
+    $bcf_type_shift : Uint8T*
+    $bcf_float_vector_end : Uint32T
+    $bcf_float_missing : Uint32T
   end
 end
