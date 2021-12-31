0b153feb534ef2d20624e545006cfb049bf74d11
kojix2 <2xijok@gmail.com>
2021-12-31 09:40:48 +0900

    Enable BamRecord test

--- test/hts/bam/record_test.cr
+++ test/hts/bam/record_test.cr
@@ -2,19 +2,22 @@ require "minitest/autorun"
 require "../../../src/hts/bam"
 
 class BamRecordTest < Minitest::Test
-  # def teardown
-  #   @bam.try &.close
-  # end
-
   def test_bam_path
-    File.expand_path("../fixtures/poo.sort.bam", __DIR__)
+    File.expand_path("../../fixtures/poo.sort.bam", __DIR__)
   end
 
   def aln1 : HTS::Bam::Record
-    @aln1 ||= HTS::Bam.new(test_bam_path).first
+    bam = HTS::Bam.new(test_bam_path)
+    r = bam.first
+    bam.close
+    r
   end
 
   def test_qname
-    assert_equal "poo", aln1.try &.qname
+    assert_equal "poo_3290_3833_2:0:0_2:0:0_119", aln1.qname
+  end
+
+  def test_mate_chrom
+    assert_equal "poo", aln1.mate_chrom
   end
 end
