a5b5f45ffe1753680d593574d41f2fc8496691df
kojix2 <2xijok@gmail.com>
2021-12-31 18:46:35 +0900

    Add tests to Bcf::Record

--- test/hts/bcf/record_test.cr
+++ test/hts/bcf/record_test.cr
@@ -2,4 +2,43 @@ require "minitest/autorun"
 require "../../../src/hts/bcf"
 
 class BcfRecordTest < Minitest::Test
+  def test_bcf_path
+    File.expand_path("../../fixtures/test.bcf", __DIR__)
+  end
+
+  def var1 : HTS::Bcf::Record
+    bcf = HTS::Bcf.new(test_bcf_path)
+    v = bcf.first
+    bcf.close
+    v
+  end
+
+  def test_pos
+    assert_equal 2126, var1.pos
+  end
+
+  def test_start
+    assert_equal 2125, var1.start
+  end
+
+  def test_stop
+    assert_equal 2126, var1.stop
+  end
+
+  def test_id
+    assert_equal ".", var1.id
+  end
+
+  def test_qual
+    assert_equal 142.41574_f32, var1.qual
+  end
+
+  def test_ref
+    assert_equal "T", var1.ref
+  end
+
+  def test_s
+    assert_equal "poo\t2126\t.\tT\tC\t142.416\t.\tDP=31;VDB=0.673439;SGB=-0.69311;MQSBZ=0;FS=0;MQ0F=0;AC=2;AN=2;DP4=0,0,14,17;MQ=60\tGT:PL\t1/1:172,93,0\n",
+     var1.to_s
+  end
 end
