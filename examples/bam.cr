3784e36601d117f68334fa06da23ae4304190d8c
kojix2 <2xijok@gmail.com>
2022-01-19 11:38:10 +0900

    Fix example

--- examples/bam.cr
+++ examples/bam.cr
@@ -6,8 +6,8 @@ HTS::Bam.open(bam_path) do |b|
   b.each do |r|
     p name: r.qname,
       flag: r.flag.value,
-      start: r.start + 1,
-      mpos: r.mate_pos + 1,
+      pos: r.start + 1,
+      mpos: r.mate_start + 1,
       mqual: r.mapping_quality,
       seq: r.sequence,
       cigar: r.cigar,
