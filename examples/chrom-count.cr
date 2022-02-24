be863143529dad919713280c036f9fb355238d04
kojix2 <2xijok@gmail.com>
2022-02-24 12:13:41 +0900

    Add chrom-cout example

---
+++ examples/chrom-count.cr
@@ -0,0 +1,35 @@
+require "htslib/hts/bam"
+require "option_parser"
+
+nthreads = 0
+
+OptionParser.parse do |parser|
+  parser.banner = "Usage: chrom-count [options] <bam_file>"
+  parser.on("-t NUM", "--threads NUM") { |v| nthreads = v.to_i }
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
+  parser.invalid_option do
+    puts parser
+    exit 1
+  end
+  if ARGV.empty?
+    puts parser
+    exit 1
+  end
+end
+
+fname = ARGV[0]
+
+bam = HTS::Bam.open(fname, threads: nthreads, index: false)
+
+chroms = bam.map do |r|
+  chr = r.chrom
+  chr == "" ? "*" : chr
+end
+
+puts chroms.tally
+           .map { |i| i.join("\t") }
+           .join("\n")
+
