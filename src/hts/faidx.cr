78252391aeefb06f49fda55702c13e66f82b2974
kojix2 <2xijok@gmail.com>
2022-07-31 19:29:54 +0900

    Add Faidx

--- src/hts/faidx.cr
+++ src/hts/faidx.cr
@@ -3,5 +3,80 @@ require "./version"
 
 module HTS
   class Faidx
+    getter :file_name
+
+    def self.open(file_name : Path | String)
+      new(file_name)
+    end
+
+    def self.open(file_name : Path | String)
+      file = new(file_name)
+      begin
+        yield file
+      ensure
+        file.close
+      end
+      file
+    end
+
+    def initialize(file_name : Path | String)
+      @file_name = file_name.to_s || ""
+      @fai = LibHTS.fai_load(file_name)
+      raise "Failed to load fai file: #{file_name}" if @fai.null?
+    end
+
+    def struct
+      @fai
+    end
+
+    def close
+      LibHTS.fai_destroy(@fai)
+    end
+
+    def closed?
+      @fai.null?
+    end
+
+    def length
+      LibHTS.faidx_nseq(@fai)
+    end
+
+    def size
+      length
+    end
+
+    def chrom_size(chrom : String | Symbol)
+      chrom = chrom.to_s || ""
+      result = LibHTS.faidx_seq_len(@fai, chrom)
+    end
+
+    def chrom_names
+      Array.new(length) do |i|
+        String.new(LibHTS.faidx_iseq(@fai, i))
+      end
+    end
+
+    def seq(name : String | Symbol, start : Number, stop : Number)
+      name = name.to_s || ""
+      rlen = Pointer(Int32).malloc
+      result = LibHTS.faidx_fetch_seq(@fai, name, start, stop, rlen)
+      case rlen.value
+      when -2 then raise "Invalid chromosome name: #{name}"
+      when -1 then raise "Error fetching sequence: #{name}:#{start}-#{stop}"
+      end
+      String.new(result)
+    end
+
+    def seq(name : String | Symbol)
+      name = name.to_s || ""
+      rlen = Pointer(Int32).malloc
+      result = LibHTS.fai_fetch(@fai, name, rlen)
+      case rlen.value
+      when -2 then raise "Invalid chromosome name: #{name}"
+      when -1 then raise "Error fetching sequence: #{name}"
+      end
+      String.new(result)
+    end
+
   end
 end
--- test/hts/faidx_test.cr
+++ test/hts/faidx_test.cr
@@ -2,4 +2,60 @@ require "minitest/autorun"
 require "../../src/hts/faidx"
 
 class FaidxTest < Minitest::Test
+  def teardown
+    @faidx.try &.close
+  end
+
+  def test_faidx_path
+    File.expand_path("../fixtures/random.fa", __DIR__)
+  end
+
+  def faidx
+    @faidx ||= HTS::Faidx.new(test_faidx_path)
+  end
+
+  def test_new
+    f = HTS::Faidx.new(test_faidx_path)
+    assert_instance_of HTS::Faidx, f
+    f.close
+  end
+
+  def test_open
+    f = HTS::Faidx.open(test_faidx_path)
+    assert_instance_of HTS::Faidx, f
+    f.close
+  end
+
+  def test_open_with_block
+    f = HTS::Faidx.open(test_faidx_path) do |f|
+      assert_instance_of HTS::Faidx, f
+    end
+  end
+
+  def test_faidx_file_name
+    assert_equal test_faidx_path, faidx.file_name
+  end
+
+  def test_struct
+    assert_instance_of HTS::LibHTS::FaidxT, faidx.struct
+  end
+
+  def test_size
+    assert_equal 5, faidx.size
+  end
+
+  def test_length
+    assert_equal 5, faidx.length
+  end
+
+  def test_chrom_names
+    assert_equal ["chr1", "chr2", "chr3", "chr4", "chr5"], faidx.chrom_names
+  end
+
+  def test_seq
+    assert_equal "TTGTGGAGAC", faidx.seq("chr1:1-10")
+    assert_equal "TTGTGGAGAC", faidx.seq(:chr1, 0, 9)
+    assert_equal "ACTTAGTTGA", faidx.seq("chr2:11-20")
+    assert_equal "ACTTAGTTGA", faidx.seq(:chr2, 10, 19)
+  end
 end
