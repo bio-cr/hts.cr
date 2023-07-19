bb3c21ecd11635547964e049564b9b8a1a902f71
kojix2 <2xijok@gmail.com>
2023-07-19 20:20:26 +0900

    Add methods to BCF::Header

--- src/hts/bcf/header.cr
+++ src/hts/bcf/header.cr
@@ -22,6 +22,10 @@ module HTS
         String.new LibHTS.bcf_hdr_get_version(@bcf_hdr)
       end
 
+      def set_version(version)
+        LibHTS.bcf_hdr_set_version(@bcf_hdr, version)
+      end
+
       def nsamples
         LibHTS2.bcf_hdr_nsamples(@bcf_hdr)
       end
@@ -31,6 +35,32 @@ module HTS
         Array.new(nsamples) do |i|
           String.new @bcf_hdr.value.samples[i]
         end
+      end
+
+      def add_sample(sample, sync : Bool = true)
+        LibHTS.bcf_hdr_add_sample(@bcf_hdr, sample)
+        self.sync if sync
+      end
+
+      def merge(hdr)
+        LibHTS.bcf_hdr_merge(@bcf_hdr, hdr)
+      end
+
+      def sync
+        LibHTS.bcf_hdr_sync(@bcf_hdr)
+      end
+      
+      def read_bcf(fname)
+        LibHTS.bcf_hdr_set(@bcf_hdr, fname)
+      end
+
+      def append(line)
+        LibHTS.bcf_hdr_append(@bcf_hdr, line)
+      end
+
+      def delete(bcf_hl_type, key)
+        type = bcf_hl_type_to_int(bcf_hl_type)
+        LibHTS.bcf_hdr_remove(@bcf_hdr, type, key)
       end
 
       def to_s(io : IO)
@@ -47,6 +77,26 @@ module HTS
 
       def finalize
         LibHTS.bcf_hdr_destroy(@bcf_hdr) unless @bcf_hdr.null?
+      end
+
+      private def bcf_hl_type_to_int(bcf_hl_type)
+        return bcf_hl_type if bcf_hl_type.is_a?(Int32)
+        case bcf_hl_type.to_s.upcase
+        when "FILTER", "FIL"
+          LibHTS2::BCF_HL_FLT
+        when "INFO"
+          LibHTS2::BCF_HL_INFO
+        when "FORMAT", "FMT"
+          LibHTS2::BCF_HL_FMT
+        when "CONTIG", "CTG"
+          LibHTS2::BCF_HL_CTG
+        when "STRUCTURED", "STR"
+          LibHTS2::BCF_HL_STR
+        when "GENOTYPE", "GEN"
+          LibHTS2::BCF_HL_GEN
+        else
+          raise "Unknown bcf_hl_type: #{bcf_hl_type}"
+        end
       end
     end
   end
--- test/hts/bcf/header_test.cr
+++ test/hts/bcf/header_test.cr
@@ -28,12 +28,36 @@ class BcfHeaderTest < Minitest::Test
     assert_equal "VCFv4.2", hdr.get_version
   end
 
+  def test_set_version
+    hdr2 = hdr.clone
+    hdr2.set_version("VCFv9.9")
+    assert_equal "VCFv9.9", hdr2.get_version
+  end
+
   def test_nsamples
     assert_equal 1, hdr.nsamples
   end
 
   def test_samples
     assert_equal ["poo.sort.bam"], hdr.samples
+  end
+
+  def test_sync
+    hdr2 = hdr.clone
+    hdr2.add_sample("kojix1", sync: false)
+    hdr2.add_sample("kojix2", sync: false)
+    hdr2.add_sample("kojix3", sync: false)
+    assert_equal 1, hdr2.nsamples
+    assert_equal ["poo.sort.bam"], hdr2.samples
+    hdr2.sync
+    assert_equal 4, hdr2.nsamples
+    assert_equal ["poo.sort.bam", "kojix1", "kojix2", "kojix3"], hdr2.samples
+  end
+
+  def test_append_delete
+    h = HTS::Bcf::Header.new
+    h.append("##FILTER=<ID=Nessie,Description=\"Nessie is a creature in Scottish folklore that is said to inhabit Loch Ness in the Scottish Highlands.\">")
+    h.delete("FILTER", "Nessie")
   end
 
   def test_to_s
