fb34c98e98682e1125ea93bf6e36f1072984c422
kojix2 <2xijok@gmail.com>
2025-11-03 15:26:53 +0900

    Fix again

--- src/hts/libhts/libhts.cr
+++ src/hts/libhts/libhts.cr
@@ -1099,7 +1099,7 @@ module HTS
     # - seq_nt16_str:   code -> ASCII string mapping ("=ACMGRSVTWYHKDBN")
     # - seq_nt16_int:   legacy integer mapping table
     $seq_nt16_table : UInt8[256]
-    $seq_nt16_str : LibC::Char*
+    $seq_nt16_str : LibC::Char[16]
     $seq_nt16_int : Int32[16]
 
     # HTS Expression/Filter functions (hts_expr.h)
