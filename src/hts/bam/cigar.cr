2fde6f7202f6db1b4d79eb0c053adf8c946fd9d7
kojix2 <2xijok@gmail.com>
2025-11-03 11:16:08 +0900

    CIGAR: Add encode methods for converting operations and CIGAR strings to UInt32

--- src/hts/bam/cigar.cr
+++ src/hts/bam/cigar.cr
@@ -26,6 +26,35 @@ module HTS
         end
       end
 
+      # Encode helpers
+      #
+      # Supported op chars follow htslib: M I D N S H P = X B
+      # Returns an Array of UInt32 where each element packs len/op as in BAM.
+      def self.encode(ops : Array(Tuple(Char, UInt32))) : Array(UInt32)
+        Array(UInt32).new(ops.size) do |i|
+          op_char, len = ops[i]
+          (len << 4) | op_code(op_char)
+        end
+      end
+
+      # Parse a CIGAR string like "10M1I5M" into encoded UInt32 words.
+      def self.encode(cigar_str : String) : Array(UInt32)
+        res = [] of UInt32
+        num = 0_u32
+        cigar_str.each_char do |ch|
+          if ch.ascii_number?
+            num = num * 10 + (ch.ord - '0'.ord)
+          else
+            raise ArgumentError.new("Invalid CIGAR: length missing before '#{ch}'") if num == 0
+            code = op_code(ch)
+            res << ((num << 4) | code)
+            num = 0
+          end
+        end
+        raise ArgumentError.new("Invalid CIGAR: trailing length without op") unless num == 0
+        res
+      end
+
       # Map CIGAR op char -> op code (htslib BAM op codes)
       private def self.op_code(ch : Char) : UInt32
         case ch
