84bb3948eaaad08b06f74ae5ce5dd0b0483a03ed
kojix2 <2xijok@gmail.com>
2025-11-03 11:11:18 +0900

    CIGAR: Move op_code mapping to a private method for better organization

--- src/hts/bam/cigar.cr
+++ src/hts/bam/cigar.cr
@@ -25,6 +25,24 @@ module HTS
           yield({op, len})
         end
       end
+
+      # Map CIGAR op char -> op code (htslib BAM op codes)
+      private def self.op_code(ch : Char) : UInt32
+        case ch
+        when 'M' then 0_u32
+        when 'I' then 1_u32
+        when 'D' then 2_u32
+        when 'N' then 3_u32
+        when 'S' then 4_u32
+        when 'H' then 5_u32
+        when 'P' then 6_u32
+        when '=' then 7_u32
+        when 'X' then 8_u32
+        when 'B' then 9_u32
+        else
+          raise ArgumentError.new("Unknown CIGAR op: #{ch}")
+        end
+      end
     end
   end
 end
